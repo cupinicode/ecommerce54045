513536efe18db40681b5a8a98143dc2b517f74f8
Jorge Cupini <jorge.cupini@bue.edu.ar>
2024-03-10 01:14:45 -0300

    Practica Clase 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import './App.css'
+import MercadoLibre from "./components/MercadoLibre/MercadoLibre"
 import { useState, useEffect } from "react" // un array.  [1] estado   [2] funcion que lo cambia
-import NavBar from './components/NavBar/NavBar'
-import Button from './components/Button/Button'
-import ItemCount from './components/ItemCount/ItemCount'
-import ContadorDeVueltas from './components/ContadorDeVueltas'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
-function App() {
+const  App = () => {
   const [show, setShow] = useState(false)
 
   return (
     <>
-      <NavBar/>
-      <ItemListContainer greeting={ 'Bienvenidos !!!!!!!' } />
-{/*   <ContadorDeVueltas />    <button onClick={()=> setShow(estadoAnterior => !estadoAnterior)}>{show ? 'Ocultar' : 'Mostrar'}</button>
-      {show ? <ItemCount initVal = {1}/> : null} 
- */}    </>
+      <MercadoLibre />
+    </>
   )
   
 }
--- src/components/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import classes from './Button.module.css'
-
-const Button = (props) => {
-    return <button onClick={props.callback} className={classes.buttonColor}> {props.label} </button>
-}
-
-export default Button
